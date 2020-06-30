9640ba300ce66b3fc1ce74309bbab2673cdbbd00
Fabian <fab.hof@gmx.de>
2020-06-30 15:51:47 +0200

    add comment to nb

--- code/constraint_matrix.py
+++ code/constraint_matrix.py
@@ -12,7 +12,7 @@ import pandas as pd
 from helpers import get_linear_system, noisy_lopf
 import networks
 
-n = networks.n1_t9_g2_w()
+n = networks.n2_t1_g2_w()
 noisy_lopf(n)
 
 print(n.generators_t.p)
@@ -24,5 +24,10 @@ A_, A_inv, c, x, d, m, r = (s[k].round(2) for k in
 print(r)
 
 
+C = r.mul(c, 0)
+
 def pinv(df):
-    return pd.DataFrame(np.linalg.pinv(df), *df.T.axes)
+    return pd.DataFrame(np.linalg.pinv(df), *df.T.axes).round(3)
+
+def inv(df):
+    return pd.DataFrame(np.linalg.inv(df), *df.T.axes).round(3)
