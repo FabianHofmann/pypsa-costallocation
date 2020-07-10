6fc4d5bf1eba1330de5e1a81a132b061d8480248
Fabian <fab.hof@gmx.de>
2020-07-10 11:55:48 +0200

    adjust network for analysis

--- code/dist_cost_large.py
+++ code/dist_cost_large.py
@@ -15,23 +15,7 @@ from config import to_symbol, color, to_total_symbol
 
 plt.rc("text", usetex=True)
 
-
-n = pypsa.Network("../elec_s_50_ec_lvopt_Co2L-3H.nc")
-n.calculate_dependent_values()
-n.snapshot_weightings[:] = 1
-n.generators["p_nom_extendable"] = True
-n.lines["s_nom_min"] = 0
-# n.lines['s_nom_max'] = 6000
-# n.generators['p_nom_max'] = np.inf
-n.mremove("StorageUnit", n.storage_units.index)
-# n.mremove("Link", n.links.index)
-n.mremove("Line", n.lines.query("x_pu == inf").index)
-n.remove("Bus", "DE0 0")
-
-T = 10
-n.set_snapshots(n.snapshots[:T])
-n.global_constraints.loc["CO2Limit", "constant"] *= T / 8700
-n.lopf(pyomo=False, keep_shadowprices=True, solver_name="gurobi")
+n = pypsa.Network('../solved_germany_50.nc')
 tag = "_large"
 
 
@@ -74,46 +58,46 @@ fig.savefig(f"../figures/network{tag}.png")
 # %% Allocation plot
 
 
-fig, axes = plt.subplots(3, 1, figsize=(12, 7), sharex=True)
+# fig, axes = plt.subplots(3, 1, figsize=(12, 7), sharex=True)
 
-for sn, ax in zip(n.snapshots, axes.ravel()):
-    ca_df = payments.sel(snapshot=sn, drop=True).to_dataframe()
-    dc_df = dc.sel(snapshot=sn, drop=True).to_dataframe()
-    d = dict(stacked=True, zorder=3, width=0.3, legend=False, ax=ax)
+# for sn, ax in zip(n.snapshots, axes.ravel()):
+#     ca_df = payments.sel(snapshot=sn, drop=True).to_dataframe()
+#     dc_df = dc.sel(snapshot=sn, drop=True).to_dataframe()
+#     d = dict(stacked=True, zorder=3, width=0.3, legend=False, ax=ax)
 
-    ca_df.plot.bar(position=1.1, color=color[ca_df.columns], **d)
-    dc_df.plot.bar(position=0, color=color[dc_df.columns], **d)
+#     ca_df.plot.bar(position=1.1, color=color[ca_df.columns], **d)
+#     dc_df.plot.bar(position=0, color=color[dc_df.columns], **d)
 
-    ax.set_xlim(left=-0.5)
-    ax.ticklabel_format(axis="y", style="sci", scilimits=(5, 2))
-    ax.set_xlabel("")
-    ax.set_title(f"$t = %i$" % (sn.hour + 1))
-#     ax.grid(axis="y", zorder=1, linestyle="dashed")
+#     ax.set_xlim(left=-0.5)
+#     ax.ticklabel_format(axis="y", style="sci", scilimits=(5, 2))
+#     ax.set_xlabel("")
+#     ax.set_title(f"$t = %i$" % (sn.hour + 1))
+# #     ax.grid(axis="y", zorder=1, linestyle="dashed")
 
-handles, labels = ax.get_legend_handles_labels()
-labels = [to_symbol[l] for l in labels]
-fig.legend(
-    handles[:-1],
-    labels[:-1],
-    ncol=4,
-    frameon=False,
-    loc="lower center",
-    bbox_to_anchor=(0.3, 1),
-    fontsize="large",
-    title="Flow Based (left bars)",
-)
-fig.legend(
-    handles[-1:],
-    labels[-1:],
-    ncol=1,
-    frameon=False,
-    loc="lower center",
-    bbox_to_anchor=(0.7, 1),
-    fontsize="large",
-    title="LMP Based (right bars)",
-)
-fig.tight_layout()
-fig.savefig(f"../figures/compare_allocation{tag}.png", bbox_inches="tight")
+# handles, labels = ax.get_legend_handles_labels()
+# labels = [to_symbol[l] for l in labels]
+# fig.legend(
+#     handles[:-1],
+#     labels[:-1],
+#     ncol=4,
+#     frameon=False,
+#     loc="lower center",
+#     bbox_to_anchor=(0.3, 1),
+#     fontsize="large",
+#     title="Flow Based (left bars)",
+# )
+# fig.legend(
+#     handles[-1:],
+#     labels[-1:],
+#     ncol=1,
+#     frameon=False,
+#     loc="lower center",
+#     bbox_to_anchor=(0.7, 1),
+#     fontsize="large",
+#     title="LMP Based (right bars)",
+# )
+# fig.tight_layout()
+# fig.savefig(f"../figures/compare_allocation{tag}.png", bbox_inches="tight")
 
 #%% LMP
 
--- code/solve_network.py
+++ code/solve_network.py
@@ -11,14 +11,26 @@ import numpy as np
 
 name = 'elec_s_50_ec_lvopt_Co2L-3H'
 n = pypsa.Network(f"../{name}.nc")
+mu_co2 = 100
+
+
 n.calculate_dependent_values()
+n.generators.p_nom_max.update(n.generators.query('not p_nom_extendable').p_nom)
 n.generators["p_nom_extendable"] = True
 n.lines["s_nom_min"] = 0
-# n.lines['s_nom_max'] = 6000
-# n.generators['p_nom_max'] = np.inf
 n.mremove("StorageUnit", n.storage_units.index)
 n.mremove("Line", n.lines.query("x_pu == inf").index)
 n.remove("Bus", "DE0 0")
+
+em = n.generators.carrier.map(n.carriers.co2_emissions) / \
+        n.generators.efficiency
+n.global_constraints = n.global_constraints.drop('CO2Limit')
+n.generators['marginal_cost'] += em * mu_co2
+
+
 n.lopf(pyomo=False, keep_shadowprices=True, solver_name="gurobi",
        solver_options={'crossover': 0, 'method': 2})
-n.to_netcdf('solved_germany_50.nc')
+
+# reinsert mu
+n.global_constraints.at['CO2Limit', 'mu'] = mu_co2
+n.export_to_netcdf('../solved_germany_50.nc')
