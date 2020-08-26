2aba3dff046992e5a97a76bf7111eedd332a614d
Fabian <fab.hof@gmx.de>
2020-08-26 18:07:38 +0200

    paper, last chapter update install.sh for custom power plants

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ git clone https://github.com/PyPSA/technology-data.git
 # cp technology-data/outputs/cost_2030.csv pypsa-de/data/costs.csv
 
 # conda install environment.yaml
+
+# python 
+# '''
+# import powerplantmatching as pm
+# pm.data.OPSD_VRE_country("DE").to_csv("pypsa-de/data/custom_powerplants.csv")
+# '''
